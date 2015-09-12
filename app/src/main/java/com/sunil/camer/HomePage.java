b41652dfd45ba3ff5008c32e05229d3891318aa8
nuron142 <sunilcrj1@outlook.com>
2015-09-12 16:29:54 +0530

    GeoInfo changes

--- app/src/main/java/com/sunil/camer/HomePage.java
+++ app/src/main/java/com/sunil/camer/HomePage.java
@@ -1,15 +1,20 @@
 package com.sunil.camer;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
+import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
@@ -18,17 +23,31 @@ import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.math.RoundingMode;
 import java.net.URI;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Locale;
 
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
+    double myLat=0, myLong = 0;
 
     public static final int CAPTURE_IMAGE_FULLSIZE_ACTIVITY_REQUEST_CODE = 1777;
     private static final int REQUEST_CAMERA = 0;
@@ -90,6 +109,79 @@ public class HomePage extends AppCompatActivity {
         }
 
     }
+
+
+    // Check connectivity
+    public boolean isNetConnected()
+    {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    //region Google Locatioin Api
+    @Override
+    public void onConnected(Bundle connectionHint) {
+
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            myLat = mLastLocation.getLatitude();
+            myLong = mLastLocation.getLongitude();
+
+            DecimalFormat formatter = new DecimalFormat("##.####", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+            formatter.setRoundingMode(RoundingMode.HALF_UP);
+
+            TextView myLocation = (TextView) findViewById(R.id.textview);
+            myLocation.setText("Lat = " + formatter.format(mLastLocation.getLatitude()) + " Long = " + formatter.format(mLastLocation.getLongitude()));
+        }
+    }
+
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.d("1", "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.d("1", "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+    //endregion
+
+    //region Activity overrides
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (!mGoogleApiClient.isConnecting() || !mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    public void onStop() {
+        if (mGoogleApiClient.isConnecting() || mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+    //endregion
 
 
 }
