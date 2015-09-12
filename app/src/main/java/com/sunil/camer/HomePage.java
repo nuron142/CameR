9499d3829af0d7ed2b8e911ee0b6615e70fbfc0f
Sunil <sunil.k@swiggy.in>
2015-09-12 15:57:21 +0530

    Camera changes included

--- app/src/main/java/com/sunil/camer/HomePage.java
+++ app/src/main/java/com/sunil/camer/HomePage.java
@@ -2,28 +2,37 @@ package com.sunil.camer;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageView;
 
-import com.desmond.squarecamera.CameraActivity;
-import com.desmond.squarecamera.ImageUtility;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
 
 public class HomePage extends AppCompatActivity {
 
+    public static final int CAPTURE_IMAGE_FULLSIZE_ACTIVITY_REQUEST_CODE = 1777;
     private static final int REQUEST_CAMERA = 0;
-
-    private Point mSize;
+    File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,33 +49,47 @@ public class HomePage extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        Display display = getWindowManager().getDefaultDisplay();
-        mSize = new Point();
-        display.getSize(mSize);
+
+
+        View view = findViewById(R.id.content_view);
+
+        Button button = (Button) view.findViewById(R.id.button_camera);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    runCameraProgram();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
     }
 
-    @Override
+    private void runCameraProgram() throws IOException {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "SUNIL_" + timeStamp + "_img";
+
+        file = new File(getExternalFilesDir(null), imageFileName + ".jpg");
+
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        startActivityForResult(intent, CAPTURE_IMAGE_FULLSIZE_ACTIVITY_REQUEST_CODE);
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode != RESULT_OK) return;
+        if (requestCode == CAPTURE_IMAGE_FULLSIZE_ACTIVITY_REQUEST_CODE) {
+            if (file.exists())
+                ((ImageView) findViewById(R.id.image)).setImageURI(Uri.fromFile(file));
 
-        if (requestCode == REQUEST_CAMERA) {
-            Uri photoUri = data.getData();
-
-            if(data.getData() == null)
-                Log.d("1","data null");
-            else
-                Log.d("1","data NOT null");
-
-
-            // Get the bitmap in according to the width of the device
-            Bitmap bitmap = ImageUtility.decodeSampledBitmapFromPath(photoUri.getPath(), mSize.x, mSize.x);
-            ((ImageView) findViewById(R.id.image)).setImageBitmap(bitmap);
         }
-        super.onActivityResult(requestCode, resultCode, data);
+
     }
 
-    public void launchCamera(View view) {
-        Intent startCustomCameraIntent = new Intent(this, CameraActivity.class);
-        startActivityForResult(startCustomCameraIntent, REQUEST_CAMERA);
-    }
+
 }
